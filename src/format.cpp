1f55e23373fe38d1dd3344ae3d84ffd40ec6c4f0
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-03-30 11:52:21 -0700

    Defined ElapsedTime() to return tim ein hh:mm:ss

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,5 @@
 #include <string>
+//#include <iostream>
 
 #include "format.h"
 
@@ -7,5 +8,26 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds)
+{
+    int hh;
+    int mm;
+    int ss;
+    std::string time;
+
+    hh = seconds / 3600;
+    mm = (seconds % 3600) / 60;
+    ss = (seconds % 3600) % 60;
+
+    time = std::to_string(hh) + ":" + std::to_string(mm) + ":" + std::to_string(ss);
+
+    return time;
+}
+
+//Unit Testing 
+/* The output for 12500 seconds should be 3:28:20
+int main()
+{
+    std::cout << Format::ElapsedTime(12500) << "\n";
+}
+*/
