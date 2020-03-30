45d3c0105195482e9a4faeed6998031f45eee107
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-03-29 18:55:52 -0700

    Updated system.cpp with related linux_parser function

--- src/system.cpp
+++ src/system.cpp
@@ -15,27 +15,40 @@ using std::string;
 using std::vector;
 
 // TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
+Processor &System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process> &System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel()
+{
+    return linux_parser::Kernel();
+}
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { 
-    return LinuxParser::MemoryUtilization(); 
+float System::MemoryUtilization()
+{
+    return LinuxParser::MemoryUtilization();
 }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem()
+{
+    return linux_parser::OperatingSystem();
+}
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses()
+{
+    return 0;
+}
 
 // TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return 0; }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+long int System::UpTime()
+{
+    return LinuxParser::UpTime();
+}
